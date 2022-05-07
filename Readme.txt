a4d650cb82569e4ec65f18485f868d5befb2d523
jungfocus7 <jungfocus7@gmail.com>
2022-05-07 15:09:31 +0900

    ``

--- Readme.txt
+++ Readme.txt
@@ -1,3 +1,10 @@
 Nodejs based javascript library.
 
 https://github.com/jungfocus7/jhb0b_js_libs
+
+
+
+
+
+1) >>> 참고용
+https://github.com/jungfocus7/jhb0b_as3_libs
--- js/hfstr.js
+++ js/hfstr.js
@@ -1,6 +1,39 @@
 'use strict';
 //======================================================================
-// 관련 모듈
+// # hfstr: 문자열 관련 모듈
 //======================================================================
+/**
+ *
+ * @param {*} tstr
+ * @returns
+ */
+const is_str = (tstr) => {
+    if (typeof tstr === 'string')
+        return tstr.trim() !== '';
+    else
+        return false;
+
+};
+
+
+
+/**
+ *
+ * @param {*} tstr
+ * @param {*} token
+ * @returns
+ */
+const get_lastNum = (tstr, token = '_') => {
+    const ti = tstr.lastIndexOf(token) + 1;
+    return +tstr.substr(ti);
+};
+
+
+
+
+module.exports = Object.seal({
+    is_empty,
+    get_lastNum
+});
 
 //======================================================================
