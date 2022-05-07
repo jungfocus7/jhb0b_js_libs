19fa446ab311708b01f2ec4cd6e243dac05de7c3
jungfocus7 <jungfocus7@gmail.com>
2022-05-07 09:16:26 +0900

    ``

--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,3 @@
 Nodejs based javascript library.
+
+https://github.com/jungfocus7/jhb0b_js_libs
--- js/hfarr.js
+++ js/hfarr.js
@@ -5,6 +5,87 @@
 
 
 
+/**
+ *
+ * @param {*} tarr
+ * @returns
+ */
+const is_arr = (tarr) => {
+    return Array.isArray(tarr) && (tarr.length > 0);
+    // if (!Array.isArray(tarr)) return false;
+    // return (tarr !== null) && (tarr.length > 0);
+
+};
+
+
+
+/**
+ *
+ * @param {*} tarr
+ * @param {*} te
+ * @returns
+ */
+const is_contains = (tarr, te) => {
+    if (is_arr(tarr) === false) return false;
+
+
+    let tb = false;
+
+    const tl = tarr.length
+    for (let i = 0; i < tl; i++) {
+        if (tarr[i] === te) {
+            tb = true;
+            break;
+        }
+    }
+
+    return tb;
+
+};
+
+
+
+/**
+ *
+ * @param {*} tarr
+ * @returns
+ */
+const shuffle = (tarr) => {
+    if (is_arr(tarr) === false) return;
+
+
+    const tl = tarr.length;
+    for (let i = 0; i < tl; i++) {
+        let te = tarr[i];
+        let ti = _mu_num.randRange(0, tl - 1);
+        tarr[i] = tarr[ti];
+        tarr[ti] = te;
+    }
+
+};
+
+
+
+/**
+ *
+ * @param {*} tarr
+ * @returns
+ */
+const copy = (tarr) => {
+    if (is_arr(tarr) === false) return null;
+
+    return tarr.slice();
+};
+
+
+
+
+module.exports = Object.seal({
+    is_arr,
+    is_contains,
+    shuffle,
+    copy
+});
 
 
 //======================================================================
