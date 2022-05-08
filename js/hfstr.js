dc5309fd67b86e89d0e46120c727a4f2b268b2f5
jungfocus7 <jungfocus7@gmail.com>
2022-05-08 20:12:04 +0900

    ``

--- js/hfstr.js
+++ js/hfstr.js
@@ -3,7 +3,7 @@
 // # hfstr: 문자열 관련 모듈
 //======================================================================
 /**
- *
+ * 문자열 유효성 확인
  * @param {*} tstr
  * @returns
  */
@@ -18,7 +18,7 @@ const is_str = (tstr) => {
 
 
 /**
- *
+ * 이름에서 마지막 번호 확인
  * @param {*} tstr
  * @param {*} token
  * @returns
@@ -26,6 +26,38 @@ const is_str = (tstr) => {
 const get_lastNum = (tstr, token = '_') => {
     const ti = tstr.lastIndexOf(token) + 1;
     return +tstr.substr(ti);
+
+};
+
+
+
+/**
+ * 문자열 >> ArrayBuffer 변환
+ * @param {*} tstr
+ * @returns
+ */
+const str2ab = (tstr) => {
+    const tl = tstr.length;
+
+    let tab = new Uint16Array(new ArrayBuffer(tl * 2));
+    for (let i = 0; i < tl; i++) {
+        tab[i] = tstr.charCodeAt(i);
+    }
+
+    return tab;
+
+};
+
+
+
+/**
+ * ArrayBuffer >> 문자열 변환
+ * @param {*} tab
+ * @returns
+ */
+const ab2str = (tab) => {
+    return String.fromCharCode.apply(null, tab);
+
 };
 
 
@@ -35,5 +67,6 @@ module.exports = Object.seal({
     is_empty,
     get_lastNum
 });
+
 
 //======================================================================
